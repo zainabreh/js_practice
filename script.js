fea61621288c8764b64f7ab6970ac3c246e755e8
Zainab <156816915+zainabreh@users.noreply.github.com>
2024-07-18 12:32:27 +0500

    JSON Parsing Done

--- script.js
+++ script.js
@@ -1,52 +1,37 @@
-// Pure Function
+// JSON Handling
 
-// Only depends on its input arguments
-// Only returns a value based on those arguments
-// Does not modify external state or have any side effects
-// Always returns the same output for the same inputs
+/*JSON (JavaScript Object Notation) is a lightweight data interchange format that is easy to read and write. It is a text-based format that is language-independent, making it a popular choice for exchanging data between different systems.*/
 
-const exchangeRate = 0.84;
+// JSON Parsing Methods
 
-function convertUSDToEUR(amount) {
-  return amount * exchangeRate;
-}
-
-console.log(convertUSDToEUR(100));
-console.log(convertUSDToEUR(50));
-console.log(convertUSDToEUR(200));
+// 1)  json.stringify
+// 2) json.parse
 
 
 
 
+const jsonObject = {
+    name: "John Doe",
+    age: 30,
+    occupation: "Developer",
+    address: {
+      street: "123 Main St",
+      city: "Anytown",
+      state: "CA",
+      zip: "12345"
+    },
+    interests: ["reading", "hiking", "coding"]
+  };
 
+// 1)
 
-
-
-// IIFE stands for Immediately Invoked Function Expression. It's a self-invoking anonymous function that is executed immediately after it's defined.
-
-
-(function() {
-    var random = Math.random();
-    console.log("Random number:", random*5);
-  })();
-
-
-
-
-
-
-
-
-
-// Higher Order Function
-
-// In JavaScript, a higher-order function (HOF) is a function that takes another function as an argument or returns a function as a result.
-
-
-const arr = [1, 2, 3, 4, 5];
-
-const evenArr = arr.filter((e)=> {
-  return e % 2 === 0;
-});
-
-console.log(evenArr);
+  const jsonString = JSON.stringify(jsonObject);
+  console.log("Converting JSON to String.....");
+  console.log(jsonString);
+  
+  //  2)
+  
+  const jsonParse = JSON.parse(jsonString)
+  console.log("Converting JSONStringify to JSONObject.....");
+  console.log(jsonParse);
+  
