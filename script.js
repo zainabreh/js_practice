d8403c14ae4640efba930c9afe89c7de1d72086d
Zainab <156816915+zainabreh@users.noreply.github.com>
2024-07-18 12:20:32 +0500

    Types of Function in js

--- script.js
+++ script.js
@@ -1,29 +1,52 @@
-// Event Bubbling
+// Pure Function
 
-// In this,when an event occur it start from the target element and traverse/move in the upward direction until it reaches the root element in the DOM tree.
+// Only depends on its input arguments
+// Only returns a value based on those arguments
+// Does not modify external state or have any side effects
+// Always returns the same output for the same inputs
+
+const exchangeRate = 0.84;
+
+function convertUSDToEUR(amount) {
+  return amount * exchangeRate;
+}
+
+console.log(convertUSDToEUR(100));
+console.log(convertUSDToEUR(50));
+console.log(convertUSDToEUR(200));
 
 
 
-// Event Capturing
 
-// In this,when an event occur it start from the root element of the DOM and traverse/move in the downward direction until it reaches the target element.
 
-let container = document.getElementById("container")
-let innerContainer = document.getElementById("inner-container")
-let button = document.getElementById("button")
 
-document.addEventListener('click', function(event) {
-    console.log('Document clicked!');
-  }, true); // Capture phase
-  
-  container.addEventListener('click', function(event) {
-    console.log('Container clicked!');
-  }, false); // Bubbling phase
-  
-  innerContainer.addEventListener('click', function(event) {
-    console.log('Inner container clicked!');
-  }, false); // Bubbling phase
-  
-  button.addEventListener('click', function(event) {
-    console.log('Button clicked!');
-  }, false); // Bubbling phase
+
+
+// IIFE stands for Immediately Invoked Function Expression. It's a self-invoking anonymous function that is executed immediately after it's defined.
+
+
+(function() {
+    var random = Math.random();
+    console.log("Random number:", random*5);
+  })();
+
+
+
+
+
+
+
+
+
+// Higher Order Function
+
+// In JavaScript, a higher-order function (HOF) is a function that takes another function as an argument or returns a function as a result.
+
+
+const arr = [1, 2, 3, 4, 5];
+
+const evenArr = arr.filter((e)=> {
+  return e % 2 === 0;
+});
+
+console.log(evenArr);
